bb6a0b6c4f50a6dd5c226cdc0ce9441119557f46
Muhammad Sandy <musanpras@gmail.com>
2023-09-07 14:54:26 +0700

    first commit

---
+++ src/data/info.ts
@@ -0,0 +1,104 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Muhammad Sandy Prastyo",
+  jobDescription: "Frontend Developer",
+  about: `I'm Frontend Engineer with a Bachelor's degree in computer science and over 3 years of hands-on expertise. Proficient in HTML5, CSS, Javascript, Typescript, ReactJS, Redux, Micro Frontend, Single Page Applications, and Unit Testing, demonstrating strong analytical, design, logical, and programming skills. Adept at using Front End Frameworks like Bootstrap and Tailwind, with additional proficiency in Figma. Possesses a deep understanding of the Software Development Life Cycle and incorporates secure software development practices into projects. Well-versed in version control using GIT, consistently delivering clean, maintainable, and thoroughly-tested code. Exceptional troubleshooting skills and a commitment to building seamless, high-quality user experiences.
+  `,
+
+  experience: [
+    {
+      name: "Frontend Developer",
+      location: "Tokodistributor - B2B Marketplace",
+      startDate: "Jun 2022",
+      endDate: "Sep 2023",
+      description: [
+        "- Developing screens and UI components for the web application using React, Vue, and Bootstrap",
+        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+      ],
+    },
+
+    {
+      name: "Flutter and Mobile Developer",
+      location: "Freelance (Self employed)",
+      startDate: "Jun 2022",
+      endDate: "May 2023",
+      description: [
+        "- Build and Maintain Interface and integrate with API Telepandu Apps ( Tech Stack Frontend: Flutter, Dart. Tools: Android Studio)",
+        "- Build and Maintain Interface and integrate with API Telepandu Apps ( Tech Stack Frontend: Flutter, Dart. Tools: Android Studio)",
+        "- Build Interface and Integrate a Gym App for Users and Personal Trainers with a vendor called Vizta Gym (Tech Stack Frontend: Flutter, Dart. Tools: Android Studio)"
+      ],
+    },
+
+    {
+      name: "Flutter and Mobile Developer",
+      location: "Reacteev ID",
+      startDate: "Sep 2021",
+      endDate: "Jun 2022",
+      description: [
+        "- Build interface and integrate with API a Muslim app to give daily needs information of Muslims called Lillah (Tech Stack: React Native CLI, javascript. Tools: VS Code)",
+        "- Maintaining a delivery app interface and API Integration called Daeng Kurir to integrate with Shopping Feature ( Tech Stack Frontend: Flutter, Dart. Tools: Android Studio)",
+        "- Build and Maintain Interface and integrate with API Telepandu Apps ( Tech Stack Frontend: Flutter, Dart. Tools: Android Studio)",
+      ],
+    },
+
+    {
+      name: "Project Manager",
+      location: "Kafilah Corp.",
+      startDate: "May 2021",
+      endDate: "Sep 2021",
+      description: [
+        "- Manage teams to reach the goal of the company by assigning the task, controlling deadlines, and delivering projects to stack holders ( Method: Waterfall )",
+      ],
+    },
+
+    {
+      name: "Game Developer",
+      location: "Enjineer.id",
+      startDate: "Mar 2021",
+      endDate: "May 2022",
+      description: [
+        "- Redesign and Enhancement of existing game (Tech Stack: Visual Studio, C#, blender, Unity)",
+      ],
+    },
+
+    {
+      name: "System Analyst",
+      location: "Enjineer.id",
+      startDate: "Jan 2021",
+      endDate: "Mar 2022",
+      description: [
+        "- Analyze the system requirements to build a game app",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Riau University",
+      location: "Pekanbaru, Riau",
+      startDate: "2015",
+      endDate: "2021",
+      description: [
+        "Bachelor Degree of Informatics Engineering",
+      ],
+    },
+  ],
+
+  socialMedia: {
+    // facebook: "https://web.facebook.com/sandy.prasetyo2",
+    // twitter: "https://twitter.com/musanpras",
+    github: "https://github.com/musanpras",
+    email: "https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRttNmKcjqJLCpxPPJsdKLLgqHbRDNkvxPFKKHGrCxTlLStCRhlrGpCSqHqzQHncDpbzClv",
+    linkedin: "https://www.linkedin.com/in/muhammad-sandy-prastyo/",
+  },
+
+  projects: [
+    {
+      title: "Telepandu",
+      isFeatured: true,
+      thumbnail: "https://play-lh.googleusercontent.com/xi-VvZQKAP_kb4oXNYShnJYzsgxndW7WaZ9gQ0e4pTRRGSJKERgth6aa1cKatpeZ9Fs=w240-h480-rw",
+      // githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://play.google.com/store/apps/details?id=com.telepandu.pasien&hl=en-ID",
+    },
+  ],
+};
